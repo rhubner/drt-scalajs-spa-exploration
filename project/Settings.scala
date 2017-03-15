374928ba8117cb2a056523242858b824d66642b2
Rich Birch <rich@freshcodelimited.com>
2017-03-15 17:31:54 +0000

    DRT-4503 Attempt to get flightstats azure functions working

--- project/Settings.scala
+++ project/Settings.scala
@@ -94,11 +94,11 @@ object Settings {
     "com.amazonaws" % "aws-java-sdk" % "1.11.89",
     "com.mfglabs" %% "commons-aws" % "0.10.0",
     "org.apache.commons" % "commons-csv" % "1.4",
+    "org.opensaml" % "opensaml" % "2.6.1",
+    "joda-time" % "joda-time" % "2.9.4",
     "com.vmunier" %% "scalajs-scripts" % "1.0.0",
     "com.lihaoyi" %% "pprint" % "0.4.3",
     "org.scalacheck" %% "scalacheck" % "1.13.4" % "test",
-
-  "joda-time" % "joda-time" % "2.9.4",
     "org.pac4j" % "pac4j-saml" % "2.0.0-RC1"
   ) :::
     List("io.spray" %% "spray-client" % versions.sprayVersion,
